3aa7b12d571e10570d61198c9bf32282a619c8d1
ketan0215 <94388954+ketan0215@users.noreply.github.com>
2022-07-30 16:31:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+
+});
